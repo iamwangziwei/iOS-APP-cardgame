0516f62f3166e4af21d17f033fb8c2d407910249
wangziwei <35870166+maoyiyiyi@users.noreply.github.com>
2020-12-27 15:58:38 -0500

    cardgame

--- test_app_2/ViewController.swift
+++ test_app_2/ViewController.swift
@@ -5,15 +5,50 @@
 //  Created by VV on 2020-12-26.
 //
 
-import UIKit
+import UIKit //UIKit libaray
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController {   //inherit from UIViewController
+    
+    @IBOutlet weak var leftimageview: UIImageView!
+    
+    @IBOutlet weak var rightinmageview: UIImageView!
+    
+    @IBOutlet weak var leftscorelabel: UILabel!
+    
+    @IBOutlet weak var rightscorelabel: UILabel!
+    
+    var leftscore = 0
+    var rightscore = 0
+    
+    @IBAction func dealtapped(_ sender: Any) {
+        //randomized number
+        let leftnumber = Int.random(in: 2...14)
+        let rightnumber = Int.random(in: 2...14)
+        
+        leftimageview.image = UIImage(named: "card\(leftnumber)")
+        
+        rightinmageview.image = UIImage(named: "card\(rightnumber)")
+        
+        if leftnumber > rightnumber{
+            leftscore += 1
+            leftscorelabel.text = String(leftscore)
+        }else if leftnumber < rightnumber{
+            rightscore += 1
+            rightscorelabel.text = String(rightscore)
+        }else {
+            //tie
+        }
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
     }
 
 
 }
+
 
